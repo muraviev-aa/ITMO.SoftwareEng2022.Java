cca17c1d440f0ed566f786b8babda3b068c2e3d8
muraviev-aa <muraviev-aa@yandex.ru>
2022-10-20 18:51:45 +0300

    20.10.2022

--- src/itmo/lab5/Task5.java
+++ src/itmo/lab5/Task5.java
@@ -1,4 +1,20 @@
 package itmo.lab5;
 
 public class Task5 {
+    public static void main(String[] args) {
+
+        String string = "This is a test string";
+
+        strBuilder(string);
+
+    }
+
+    public static void strBuilder(String string){
+        String[] words = new StringBuilder(string).reverse().toString().split(" ");
+        StringBuilder strb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--){
+            strb.append(words[i]).append(" ");
+        }
+        System.out.println(strb);
+    }
 }
