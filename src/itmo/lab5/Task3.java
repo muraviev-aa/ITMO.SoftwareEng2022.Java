3a3bbfe3081fa7775f8a957f9d3af5180b5e8886
muraviev-aa <muraviev-aa@yandex.ru>
2022-10-20 16:19:37 +0300

    20.10.2022

--- src/itmo/lab5/Task3.java
+++ src/itmo/lab5/Task3.java
@@ -1,4 +1,16 @@
 package itmo.lab5;
 
 public class Task3 {
+    public static void main(String[] args) {
+
+        String text = "За нами бяка и перед нами бяка, вокруг бяка. Но нам хорошо!";
+
+        replaceAll(text);
+    }
+
+    public static void replaceAll(String text) {
+
+        System.out.println(text.replace("бяка", "[вырезано цензурой]"));
+
+    }
 }
