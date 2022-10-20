f40ea4ddbb256ba52711e6e6e7181eff3cacd9a6
muraviev-aa <muraviev-aa@yandex.ru>
2022-10-20 17:31:19 +0300

    20.10.2022

--- src/itmo/lab5/Task4.java
+++ src/itmo/lab5/Task4.java
@@ -1,4 +1,26 @@
 package itmo.lab5;
 
 public class Task4 {
+    public static void main(String[] args) {
+
+        String string = "Eeeeeeeeee";
+        String substring = "ee";
+
+        subStringe(string, substring);
+
+    }
+
+    public static void subStringe(String string, String substring) {
+        int index = 0, count = 0;
+        while (true) {
+            index = string.toLowerCase().indexOf(substring, index);
+            if (index != -1) {
+                count++;
+                index = index + substring.length();
+            } else {
+                break;
+            }
+        }
+        System.out.println("Количество подстрок в строке: " + count);
+    }
 }
