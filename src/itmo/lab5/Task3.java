1012f97fb23e470c3814e896228d2a8701c89cb5
muraviev-aa <muraviev-aa@yandex.ru>
2022-10-24 17:46:06 +0300

    24.10.2022

--- src/itmo/lab5/Task3.java
+++ src/itmo/lab5/Task3.java
@@ -3,14 +3,24 @@ package itmo.lab5;
 public class Task3 {
     public static void main(String[] args) {
 
+        String changWord = "бяка";
         String text = "За нами бяка и перед нами бяка, вокруг бяка. Но нам хорошо!";
 
-        replaceAll(text);
+        replaceAll(changWord, text);
+        examSubstring(changWord, text);
     }
 
-    public static void replaceAll(String text) {
+    public static void replaceAll(String changWord, String text) {
 
-        System.out.println(text.replace("бяка", "[вырезано цензурой]"));
+        System.out.println(text.replace(changWord, "[вырезано цензурой]"));
 
+    }
+
+    public static void examSubstring(String changWord, String text) {
+        if (text.indexOf(changWord) == -1) {
+            System.out.println("Заменяемое слово не является подстрокой!");
+        }else {
+            System.out.println("Заменяемое слово является подстрокой");
+        }
     }
 }
