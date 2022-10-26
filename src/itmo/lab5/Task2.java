31a9f7c7891f20fd8ff2ac622a97e1c402d7f2ae
muraviev-aa <muraviev-aa@yandex.ru>
2022-10-26 07:40:29 +0300

    26.10.2022

--- src/itmo/lab5/Task2.java
+++ src/itmo/lab5/Task2.java
@@ -5,19 +5,14 @@ import java.util.Collections;
 public class Task2 {
     public static void main(String[] args) {
         String text = "Level";
-        char[] array = text.toLowerCase().toCharArray();
 
         isPalindromeUsingStringBuilder(text);
     }
 
     public static void isPalindromeUsingStringBuilder(String text) {
-        String clean = text.replaceAll("\\s+", "").toLowerCase();
+        String clean = text.toLowerCase();
         StringBuilder plain = new StringBuilder(clean);
         StringBuilder reverse = plain.reverse();
-        if (plain == reverse) {
-            System.out.println("Это слово палиндром");
-        } else {
-            System.out.println("Это слово не палиндром");
-        }
+        System.out.println("Слово " + text + " палиндром: " + reverse.toString().equals(clean));
     }
 }
