61f578ae630ab36437971cd1b1c755370a3d6573
muraviev-aa <muraviev-aa@yandex.ru>
2022-10-26 07:09:26 +0300

    26.10.2022

--- src/itmo/lab5/Task2.java
+++ src/itmo/lab5/Task2.java
@@ -1,18 +1,23 @@
 package itmo.lab5;
 
+import java.util.Collections;
+
 public class Task2 {
     public static void main(String[] args) {
         String text = "Level";
         char[] array = text.toLowerCase().toCharArray();
 
-        examPalindrom(array);
+        isPalindromeUsingStringBuilder(text);
     }
-    public static void examPalindrom(char[] array){
-        int l = array.length; //длина массива
-        if(array[0] != array[l-1]){
-            System.out.println("This word is not a palyndrom");
-        }else{
-            System.out.println("This word is palyndrom");
+
+    public static void isPalindromeUsingStringBuilder(String text) {
+        String clean = text.replaceAll("\\s+", "").toLowerCase();
+        StringBuilder plain = new StringBuilder(clean);
+        StringBuilder reverse = plain.reverse();
+        if (plain == reverse) {
+            System.out.println("Это слово палиндром");
+        } else {
+            System.out.println("Это слово не палиндром");
         }
     }
 }
