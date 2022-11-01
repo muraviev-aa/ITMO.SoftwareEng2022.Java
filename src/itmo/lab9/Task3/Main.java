2badc39dab30da1cdaaafe495348471640950233
muraviev-aa <muraviev-aa@yandex.ru>
2022-11-01 09:33:32 +0300

    01.11.2022

--- src/itmo/lab9/Task3/Main.java
+++ src/itmo/lab9/Task3/Main.java
@@ -13,13 +13,13 @@ public class Main {
         map.put(new User("Mike"), 2);
 
         Scanner scanner = new Scanner(System.in);
-        countBalls(map, scanner);
-    }
-
-    public static void countBalls(Map<User, Integer> map, Scanner scanner) {
-
         System.out.println("Введите имя: ");
         String name = scanner.nextLine();
+
+        countBalls(map, name);
+    }
+
+    public static void countBalls(Map<User, Integer> map, String name) {
         for (Map.Entry<User, Integer> part : map.entrySet()) {
             if (part.getKey().getName().equals(name)) {
                 System.out.println("Количество очков: " + part.getValue());
