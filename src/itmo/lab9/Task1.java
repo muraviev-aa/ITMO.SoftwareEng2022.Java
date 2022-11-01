7dcfa3932f3e04ec8edda1aeeb15c0197bd79aaf
muraviev-aa <muraviev-aa@yandex.ru>
2022-11-01 07:30:54 +0300

    01.11.2022

--- src/itmo/lab9/Task1.java
+++ src/itmo/lab9/Task1.java
@@ -6,27 +6,27 @@ public class Task1 {
 
     public static void main(String[] args) {
 
-        List<String> strings = new ArrayList<>();
-        strings.add("string1");
-        strings.add("string1");
-        strings.add("string2");
-        strings.add("string3");
-        strings.add("string4");
-        strings.add("string5");
-        strings.add("string5");
-        strings.add("string6");
-        strings.add("string7");
-        strings.add("string7");
+        List<String> list = new ArrayList<>();
+        list.add("string1");
+        list.add("string1");
+        list.add("string2");
+        list.add("string3");
+        list.add("string4");
+        list.add("string5");
+        list.add("string5");
+        list.add("string6");
+        list.add("string7");
+        list.add("string7");
 
-        System.out.println("Коллекция без дубликатов: " + deleteDublicates(strings));
-
+        delDublicates(list);
     }
 
-    //Метод принимает коллекцию и возвращает коллекцию уже без дубликатов
-    public static Set<String> deleteDublicates(List<String> strings) {
+    public static <T> void delDublicates(List<T> list) {
 
-        return new LinkedHashSet<>(strings);
+        Set<T> set = new LinkedHashSet<>(list);
+        System.out.println("Коллекция без дубликатов1: " + set);
     }
+
 
 }
 
