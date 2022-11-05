91f868fc2d646827b358073d1c3a972c1f3c0ea2
muraviev-aa <muraviev-aa@yandex.ru>
2022-11-05 22:11:51 +0300

    05.11.2022

--- src/itmo/labMultithreading/Task4/Main.java
+++ src/itmo/labMultithreading/Task4/Main.java
@@ -4,15 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Change change = new Change();
-
-        for(int i = 0; i < 10; i++) {
-            MyThreadOne threadOne = new MyThreadOne(change);
-            MyThreadTwo threadTwo = new MyThreadTwo(change);
-            threadOne.start();
-            threadTwo.start();
-        }
-
-
+        MyThread threadOne = new MyThread();
+        MyThread threadTwo = new MyThread();
+        threadOne.start();
+        threadTwo.start();
     }
 }
