ac06dacc6c281d5181868aab4747cc01f450824f
muraviev-aa <muraviev-aa@yandex.ru>
2022-11-06 09:35:16 +0300

    06.11.2022

--- src/itmo/labMultithreading/Task4/Main.java
+++ src/itmo/labMultithreading/Task4/Main.java
@@ -2,11 +2,37 @@ package itmo.labMultithreading.Task4;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        MyThread threadOne = new MyThread();
-        MyThread threadTwo = new MyThread();
-        threadOne.start();
-        threadTwo.start();
+        Object obj = new Object();
+
+        waitNotify(obj);
+    }
+
+    public static void waitNotify(Object obj) throws InterruptedException {
+
+        for (int i = 0; i <= 3; i++) {
+            new Thread(() -> {
+                synchronized (obj) {
+                    try {
+                        System.out.println(Thread.currentThread().getName() + " запускаем wait()");
+                        obj.wait();
+                    } catch (InterruptedException e) {
+                        e.getMessage();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " после notify() работаем дальше");
+                }
+            }).start();
+
+            Thread.sleep(3000);
+
+            new Thread(() -> {
+                synchronized (obj) {
+                    System.out.println(Thread.currentThread().getName() + " запускаем notify()");
+                    obj.notify();
+                    System.out.println(Thread.currentThread().getName() + " notify() ");
+                }
+            }).start();
+        }
     }
 }
