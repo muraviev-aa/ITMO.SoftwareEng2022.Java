e673d47bacc578ad1dae3e7f838de711a724eb42
muraviev-aa <muraviev-aa@yandex.ru>
2022-11-06 14:51:26 +0300

    06.11.2022

---
+++ src/itmo/labMultithreading/Task3/Counter.java
@@ -0,0 +1,14 @@
+package itmo.labMultithreading.Task3;
+
+public class Counter {
+
+    int count = 0;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void increment() {
+        count = count + 1;
+    }
+}
---
+++ src/itmo/labMultithreading/Task3/Main.java
@@ -0,0 +1,41 @@
+package itmo.labMultithreading.Task3;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Counter counter = new Counter();
+
+        Thread[] threads = new Thread[100];
+
+
+        Runnable run = () -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        };
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(run);
+        }
+
+        for (Thread thr : threads) {
+            thr.start();
+        }
+
+        while (!endThreads(threads)) {
+            Thread.sleep(200);
+        }
+
+        System.out.println(counter.getCount());
+    }
+
+    static boolean endThreads(Thread[] threads) {
+        for (Thread thr : threads) {
+            if(thr.isAlive()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
